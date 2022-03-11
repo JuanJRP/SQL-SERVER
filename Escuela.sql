3ebe05bb656c287b7e613a5d22d37ee58b02b141
JuanJRP <juanjoserestrepopabon@gmail.com>
2022-03-11 15:26:34 -0500

    Finalizados

---
+++ Escuela.sql
@@ -0,0 +1,33 @@
+select * from Student cross join Enrollment
+
+select * from Offering join Course on Offering.CourseNo = Course.CourseNo 
+
+select * from Offering join course on offering.OffLocation = 'BLM302'
+
+select * from Faculty left outer join offering on Faculty.FacSSN = Offering.FacSSN
+
+select * from faculty where FacSalary <= 40 union select * from faculty where FacSalary > 40
+
+SELECT * FROM Faculty EXCEPT select * from Faculty WHERE FacSalary >= 70
+
+SELECT * FROM Faculty where FacSalary >30 intersect select * from Faculty WHERE FacSalary < 80
+
+SELECT StdMajor, AVG(StdGPA)as promedio
+FROM Student
+GROUP BY StdMajor
+order by promedio asc
+
+SELECT OfferNo, AVG(EnrGrade)as promedio
+FROM Enrollment
+GROUP BY OfferNo
+order by promedio desc
+
+SELECT stdClass, AVG(StdGPA)as prom 
+FROM Student
+GROUP BY StdClass
+order by prom asc
+
+SELECT StdFirstName, StdCity, AVG(StdGPA) AS PROMCURS
+FROM Student WHERE StdGPA >= 3.7
+GROUP BY StdFirstName, StdCity
+ORDER BY PROMCURS DESC
